943ed8c764adcd7529dc0030d41af413c8e17abc
xDust <leonardoctk3@gmail.com>
2021-06-24 10:29:56 -0300

    finalizando layout do barcode scanner

--- lib/shared/bottom_sheet/bottom_sheet_widget.dart
+++ lib/shared/bottom_sheet/bottom_sheet_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class BottomSheet extends StatelessWidget {
@@ -22,18 +23,51 @@ class BottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: AppColors.shape,
-      child: Column(
-        children: [
-          Text(title),
-          Text(subtitle),
-          SetLabelButtons(
-              primaryLabel: primaryLabel,
-              primaryOnPressed: primaryOnPressed,
-              secondaryLabel: secondaryLabel,
-              secondaryOnPressed: secondaryOnPressed)
-        ],
+    return SafeArea(
+      child: RotatedBox(
+        quarterTurns: 1,
+        child: Material(
+          child: Container(
+            color: AppColors.shape,
+            child: Column(
+              children: [
+                Expanded(
+                    child: Container(
+                  color: Colors.black.withOpacity(0.6),
+                )),
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(40),
+                      child: Text.rich(
+                        TextSpan(
+                            text: title,
+                            style: TextStyles.buttonBoldHeading,
+                            children: [
+                              TextSpan(
+                                text: "\n$subtitle",
+                                style: TextStyles.buttonHeading,
+                              )
+                            ]),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Container(
+                      color: AppColors.stroke,
+                      height: 1,
+                    ),
+                    SetLabelButtons(
+                        enablePrimaryColor: true,
+                        primaryLabel: primaryLabel,
+                        primaryOnPressed: primaryOnPressed,
+                        secondaryLabel: secondaryLabel,
+                        secondaryOnPressed: secondaryOnPressed)
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
